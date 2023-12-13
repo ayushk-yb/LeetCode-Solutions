322ab50c31277c21c295d9d0a5a04cff21a280da
dotnetbabu <ayush06092002@gmail.com>
2023-12-13 19:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (77.83%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -16,7 +16,27 @@ class Solution
         }
     int uniquePaths(int m, int n)
     {
-        vector<vector < int>> dp(m, vector<int> (n, -1));
-        return solve(m - 1, n - 1, dp);
+        vector<int> dp(n, 1);
+        // return solve(m - 1, n - 1, dp);
+
+        // for(int i = 0; i < m; i++)
+        // {
+        //     dp[i][0] = 1;
+        // }
+
+        // for(int i = 0; i < n; i++)
+        // {
+        //     dp[0][i] = 1;
+        // }
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                dp[j] += dp[j - 1];
+            }
+        }
+
+        return dp[n - 1];
     }
 };

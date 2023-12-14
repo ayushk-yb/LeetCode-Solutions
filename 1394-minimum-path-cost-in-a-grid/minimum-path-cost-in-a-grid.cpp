5344475deadc3748625cfd6067498b4839d601c8
dotnetbabu <ayush06092002@gmail.com>
2023-12-14 16:59:33 +0530

    Time: 245 ms (84.00%) | Memory: 80.7 MB (38.93%) - LeetSync

--- 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
+++ 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
@@ -25,12 +25,35 @@ public:
     int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
+        vector<vector<int>> dp(n, vector<int>(m, 1e9));
         int ans = 1e9;
+        // for(int col = 0; col < m; col++)
+        // {
+        //     ans = min(ans, solve(0, col, n, m, grid, moveCost, dp));
+        // }
         for(int col = 0; col < m; col++)
         {
-            ans = min(ans, solve(0, col, n, m, grid, moveCost, dp));
+            dp[n - 1][col] = grid[n - 1][col];
         }
+
+        for(int row = n - 2; row >= 0; row--)
+        {
+            for(int col = 0; col < m; col++)
+            {
+                int val = grid[row][col];
+                for(int i = 0; i < m; i++)
+                {
+                    int cost = moveCost[val][i];
+                    dp[row][col] = min(dp[row][col], val + cost + dp[row + 1][i]);
+                }
+            }
+        }
+
+        for(int col = 0; col < m; col++)
+        {
+            ans = min(ans, dp[0][col]);
+        }
+
         return ans;
     }
 };

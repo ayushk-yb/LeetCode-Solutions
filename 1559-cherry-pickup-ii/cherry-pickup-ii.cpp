5778025cc69ae363cb720749636a57aae3425351
dotnetbabu <ayush06092002@gmail.com>
2023-12-16 23:03:17 +0530

    Time: 296 ms (5.13%) | Memory: 17.2 MB (50.83%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -33,10 +33,48 @@ public:
         return dp[r][c1][c2] = maxi;
     }
     int cherryPickup(vector<vector<int>>& grid) {
-        int row = grid.size();
+        int r = grid.size();
         int col = grid[0].size();
-        vector<vector<vector<int>>> dp(row, vector<vector<int>>(col, vector<int>(col, -1)));
+        vector<vector<vector<int>>> dp(r, vector<vector<int>>(col, vector<int>(col, -1)));
+        for(int c1 = 0; c1 < col; c1++)
+        {
+            for(int c2 = 0; c2 < col; c2++)
+            {
+                dp[r - 1][c1][c2] = grid[r - 1][c1];
+                if(c1 != c2)
+                    dp[r - 1][c1][c2] += grid[r - 1][c2];
+            }
+        }
 
-        return solve(0, 0, col - 1, row, col, grid, dp);
+        for(int row = r - 2; row >= 0; row--)
+        {
+            for(int c1 = 0; c1 < col; c1++)
+            {
+                for(int c2 = 0; c2 < col; c2++)
+                {
+                    int maxi = -1;
+                    for(int i = -1; i <= 1; i++)
+                    {
+                        for(int j = -1; j <= 1; j++)
+                        {
+                            int newC1 = c1 + i;
+                            int newC2 = c2 + j;
+                            
+                            if (newC1 >= 0 && newC1 < col && newC2 >= 0 && newC2 < col) 
+                            {
+                                int ans = grid[row][c1] + dp[row + 1][newC1][newC2];
+                                if (c1 != c2) 
+                                {
+                                    ans += grid[row][c2];
+                                }
+                                dp[row][c1][c2] = max(dp[row][c1][c2], ans);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][0][col - 1];
+        // return solve(0, 0, col - 1, r, col, grid, dp);
     }
 };

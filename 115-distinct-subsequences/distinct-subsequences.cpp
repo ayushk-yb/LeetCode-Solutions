5e3ffc52d4ce3bc61e88431bd2f03bd5b178c918
dotnetbabu <ayush06092002@gmail.com>
2023-12-23 20:20:24 +0530

    Time: 24 ms (80.61%) | Memory: 6.7 MB (97.09%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -7,17 +7,15 @@ public:
 
         for(int i = 1; i <= n; i++)
         {
-            vector<double> curr(m + 1, 0);
-            curr[0] = 1;
+            double last = prev[0];
             for(int j = 1; j <= m; j++)
             {
                 double temp = prev[j];
                 if(s[i - 1] == t[j - 1])
-                    curr[j] = prev[j - 1] + prev[j];
-                else
-                    curr[j] = prev[j];
+                    prev[j] = last + prev[j];
+                
+                last = temp;
             }
-            prev = curr;
         }
 
         return prev[m];

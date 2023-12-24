43a725c4edd987c251e475c524130b5c8ae345f4
dotnetbabu <ayush06092002@gmail.com>
2023-12-24 18:55:59 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (95.51%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,30 +2,32 @@ class Solution {
 public:
     int minDistance(string str1, string str2) {
         int n = str1.size(), m = str2.size();
-        // vector<vector<int>> dp(n + 1, (vector<int>(m + 1, -1)));
-        vector<int> prev(m + 1, -1);
-        vector<int> curr(m + 1, -1);
-        for(int j = 0; j <= m; j++)
-        {
-            prev[j] = j;
-        }
-        for(int i = 1; i <= n; i++)
-        {
-            curr[0] = i;
-            for(int j = 1; j <= m; j++)
-            {
-                if(str1[i - 1] == str2[j - 1])
-                {
-                    curr[j] = prev[j - 1];
-                }
-                else
-                {
-                    curr[j] = 1 + min(prev[j], min(curr[j - 1], prev[j - 1]));
-                }
-            }
-            prev = curr;
-        }        
 
-        return prev[m];
+        // if (n < m) {
+        //     swap(str1, str2);
+        //     swap(n, m);
+        // }
+
+        vector<int> dp(m + 1, -1);
+
+        for (int j = 0; j <= m; j++) {
+            dp[j] = j;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int prev = dp[0];
+            dp[0] = i;
+            for (int j = 1; j <= m; j++) {
+                int temp = dp[j];
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[j] = prev;
+                } else {
+                    dp[j] = 1 + min(prev, min(dp[j - 1], temp));
+                }
+                prev = temp;
+            }
+        }
+
+        return dp[m];
     }
 };

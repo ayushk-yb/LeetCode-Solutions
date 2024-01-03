8c2a27a293536844f8bab689902ee68fd8fe9ab5
dotnetbabu <ayush06092002@gmail.com>
2024-01-03 22:10:22 +0530

    Time: 1554 ms (18.95%) | Memory: 7 MB (71.95%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isPalindrome(int i, int j, string &s)
+    {
+        if(i >= j)
+            return true;
+        
+        if(s[i] != s[j])
+            return false;
+        
+        return isPalindrome(i + 1, j - 1, s);
+    }
+    int solve(int i, int n, string &s, vector<int> &dp)
+    {
+        if(i == n)
+            return 0;
+        
+        if(dp[i] != -1)
+            return dp[i];
+
+        int minCuts = 1e8;
+        for(int k = i; k < n; k++)
+        {
+            if(isPalindrome(i, k, s))
+            {
+                int cuts = 1 + solve(k + 1, n, s, dp);
+                minCuts = min(minCuts, cuts);
+            }
+        }
+
+        return dp[i] = minCuts;
+    }
+    int minCut(string s) {
+        int n = s.length();
+        vector<int> dp(n, -1);
+        return solve(0, n, s, dp) - 1;
+    }
+};

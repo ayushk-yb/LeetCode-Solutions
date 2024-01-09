ee6ed674d427a517c2cb6e41c24e73c062f829dd
dotnetbabu <ayush06092002@gmail.com>
2024-01-09 19:07:12 +0530

    Time: 78 ms (82.15%) | Memory: 61.5 MB (90.24%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -4,13 +4,21 @@ public:
         int n = nums.size();
         // sort(nums.begin(), nums.end());
         // unordered_set<int> st;
-        vector<bool> hash(n + 1, false);
-        for(int i = 0; i <= n; i++)
+        // vector<bool> hash(n + 1, false);
+        int slow = nums[0], fast = nums[0];
+        do
         {
-            if(hash[nums[i]])
-                return nums[i];
-            hash[nums[i]] = true;
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        fast = nums[0];
+        while(slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return 0;
+
+        return slow;
     }
 };

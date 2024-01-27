6c1478239a23430eb260c8763b4d6c5f077dd265
dotnetbabu <ayush06092002@gmail.com>
2024-01-27 23:30:29 +0530

    Time: 6 ms (91.63%) | Memory: 7.6 MB (15.89%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    bool isSafe(char c, int row, int col, vector<vector<char>> &board)
-    {
-        for(int i = 0; i < 9; i++)
-        {
-            if(board[i][col] == c)
-                return false;
-            if(board[row][i] == c)
-                return false;
+    // bool isSafe(char c, int row, int col, vector<vector<char>> &board)
+    // {
+    //     for(int i = 0; i < 9; i++)
+    //     {
+    //         if(board[i][col] == c)
+    //             return false;
+    //         if(board[row][i] == c)
+    //             return false;
             
-            int boxRow = 3 * (row / 3) + (i / 3);
-            int boxCol = 3 * (col / 3) + (i % 3);
-            if(board[boxRow][boxCol] == c)
-                return false; 
-        }
+    //         int boxRow = 3 * (row / 3) + (i / 3);
+    //         int boxCol = 3 * (col / 3) + (i % 3);
+    //         if(board[boxRow][boxCol] == c)
+    //             return false; 
+    //     }
 
+    //     return true;
+    // }
+    bool isSafe(char c, int row, int col, vector<vector<char>> &board) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c || board[row][i] == c || board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == c)
+                return false;
+        }
         return true;
     }
     bool solve(vector<vector<char>> &board)

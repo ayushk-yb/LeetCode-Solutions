8cf6d57fda492f21446b2356852e104fd678ca37
dotnetbabu <ayush06092002@gmail.com>
2024-02-08 16:17:24 +0530

    Time: 51 ms (66.18%) | Memory: 48.1 MB (38.82%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,68 +1,60 @@
+const int ALPHABET_SIZE = 26;
+
 class TrieNode {
 public:
-    unordered_map<char, TrieNode*> children;
+    TrieNode* children[ALPHABET_SIZE];
     bool isEnd;
 
     TrieNode() {
         isEnd = false;
+        for (int i = 0; i < ALPHABET_SIZE; ++i) {
+            children[i] = nullptr;
+        }
     }
 };
 
 class Trie {
 private:
     TrieNode* root;
+
 public:
     Trie() {
         root = new TrieNode();
     }
-    
+
     void insert(string word) {
         TrieNode* node = root;
-        for(auto ch: word)
-        {
-            if(!node->children.count(ch))
-            {
-                node->children[ch] = new TrieNode();
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (!node->children[index]) {
+                node->children[index] = new TrieNode();
             }
-            node = node->children[ch];
+            node = node->children[index];
         }
-
         node->isEnd = true;
     }
-    
+
     bool search(string word) {
         TrieNode* node = root;
-        for(auto ch: word)
-        {
-            if(!node->children.count(ch))
-            {
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (!node->children[index]) {
                 return false;
             }
-            node = node->children[ch];
+            node = node->children[index];
         }
-
         return node->isEnd;
     }
-    
+
     bool startsWith(string prefix) {
         TrieNode* node = root;
-        for(auto ch: prefix)
-        {
-            if(!node->children.count(ch))
-            {
+        for (char ch : prefix) {
+            int index = ch - 'a';
+            if (!node->children[index]) {
                 return false;
             }
-            node = node->children[ch];            
+            node = node->children[index];
         }
-
         return true;
     }
 };
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */

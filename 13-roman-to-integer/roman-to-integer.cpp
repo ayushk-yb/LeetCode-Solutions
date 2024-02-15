f75f68c0ec82814db253c0b8facd483db859e719
dotnetbabu <ayush06092002@gmail.com>
2024-02-15 17:21:43 +0530

    Time: 3 ms (93.40%) | Memory: 12.3 MB (29.26%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> romanValues = {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
-            {'C', 100}, {'D', 500}, {'M', 1000}            
-        };
+        vector<int> romanValues(128, 0); 
+        romanValues['I'] = 1;
+        romanValues['V'] = 5;
+        romanValues['X'] = 10;
+        romanValues['L'] = 50;
+        romanValues['C'] = 100;
+        romanValues['D'] = 500;
+        romanValues['M'] = 1000;
         int num = 0;
         int n = s.size();
         for(int i = 0; i < n; i++)

54d83d758dfd3dfcba2958b43daa4fca1106e8ef
dotnetbabu <ayush06092002@gmail.com>
2024-04-07 20:11:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (75.32%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        stack<int> stack_open, stack_star;
-
-        for (int i = 0; i < s.size(); ++i) {
+        int n = s.length();
+        int low = 0, high = 0; // Range of possible open parentheses
+        for (int i = 0; i < n; i++) {
             if (s[i] == '(') {
-                stack_open.push(i);
-            } else if (s[i] == '*') {
-                stack_star.push(i);
-            } else { 
-                if (!stack_open.empty()) {
-                    stack_open.pop();
-                } else if (!stack_star.empty()) {
-                    stack_star.pop();
-                } else {
-                    return false;
-                }
+                low++;
+                high++;
+            } else if (s[i] == ')') {
+                if (low > 0) low--;
+                high--;
+            } else { // '*'
+                if (low > 0) low--; // Treat '*' as '('
+                high++; // Treat '*' as ')'
             }
+            if (high < 0) return false; // More ')' than '('
         }
-
-        while (!stack_open.empty() && !stack_star.empty()) {
-            if (stack_open.top() > stack_star.top()) {
-                return false;
-            }
-            stack_open.pop();
-            stack_star.pop();
-        }
-
-        return stack_open.empty();
+        return low == 0; // All '(' are matched
     }
 };

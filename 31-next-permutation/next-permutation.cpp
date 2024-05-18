d56f3fe51f54cf53bae20c4a9881ee7d38c575ec
dotnetbabu <ayush06092002@gmail.com>
2024-05-18 19:29:39 +0530

    Time: 9 ms (14.94%) | Memory: 14.7 MB (14.81%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       int brk_pt = -1;
-       int n = nums.size();
-       for(int i = n - 2; i >= 0; i--)
-       {
-           if(nums[i] < nums[i + 1])
-           {
-               brk_pt = i;
-               break;
-           }
-       }
-       if(brk_pt == -1)
-       {
-           reverse(nums.begin(), nums.end());
-           return;
-       }
-        int i = brk_pt;
-        while(i < n - 1)
-        {
-            if(nums[i + 1] > nums[brk_pt])
-            {
-                i++;
-            }
-            else
-            {
+        int n = nums.size();
+        int breakPoint = -1;
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] < nums[i + 1]){
+                breakPoint = i;
                 break;
             }
         }
-       swap(nums[i], nums[brk_pt]);
-       reverse(nums.begin() + brk_pt + 1, nums.end());
+
+        if(breakPoint == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        int pivot = breakPoint;
+        while(pivot < n - 1){
+            if(nums[pivot + 1] > nums[breakPoint]){
+                pivot++;
+            }
+            else{
+                break;
+            }
+        }
+
+        swap(nums[pivot], nums[breakPoint]);
+        reverse(nums.begin() + breakPoint + 1, nums.end());
+        return;
     }
 };

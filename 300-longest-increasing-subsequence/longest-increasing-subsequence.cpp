0593ffd3765a20e0509b41fca8320b393cad6eb6
dotnetbabu <ayush06092002@gmail.com>
2024-05-28 16:35:49 +0530

    Time: 141 ms (34.46%) | Memory: 12.7 MB (80.90%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -7,16 +7,16 @@ public:
         vector<int> next(n + 1, 0);
 
         for (int i = n - 1; i >= 0; i--) {
-            vector<int> curr(n + 1, 0);
+            // vector<int> curr(n + 1, 0);
             for (int prev = i - 1; prev >= -1; prev--) {
                 int pick = 0;
                 if (prev == -1 || nums[i] > nums[prev]) {
                     pick = 1 + next[i + 1];
                 }
                 int not_pick = next[prev + 1];
-                curr[prev + 1] = max(pick, not_pick);
+                next[prev + 1] = max(pick, not_pick);
             }
-            next = curr;
+            // next = curr;
         }
 
         return next[0];

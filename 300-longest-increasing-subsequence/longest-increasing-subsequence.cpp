dda7d0ec098aed3c1bfe71cd9b180a1a65836247
dotnetbabu <ayush06092002@gmail.com>
2024-05-28 16:30:19 +0530

    Time: 264 ms (24.39%) | Memory: 294.6 MB (16.76%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int solve(int i, int prev, vector<int> &nums, vector<vector<int>> &dp){
-        if(i == 0){
-            if(prev == -1 || nums[i] < nums[prev]){
-                return 1;
-            }
-            return 0;
-        }
-
-        if(dp[i][prev + 1] != -1){
-            return dp[i][prev + 1];
-        }
-
-        int pick = -1e8;
-        if(prev == -1 || nums[i] < nums[prev]){
-            pick = 1 + solve(i - 1, i, nums, dp);
-        }
-
-        int not_pick = solve(i - 1, prev, nums, dp);
-
-        return dp[i][prev + 1] = max(pick, not_pick);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solve(n - 1, -1, nums, dp);
+        if (n == 0) return 0;
+        
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int prev = i - 1; prev >= -1; prev--) {
+                int pick = 0;
+                if (prev == -1 || nums[i] > nums[prev]) {
+                    pick = 1 + dp[i + 1][i + 1];
+                }
+                int not_pick = dp[i + 1][prev + 1];
+                dp[i][prev + 1] = max(pick, not_pick);
+            }
+        }
+
+        return dp[0][0];
     }
 };

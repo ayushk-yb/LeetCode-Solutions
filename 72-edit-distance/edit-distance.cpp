ca12d4a350c35b543660c967bf7110528a4efd47
dotnetbabu <ayush06092002@gmail.com>
2024-05-29 11:20:21 +0530

    Time: 4 ms (82.87%) | Memory: 9.1 MB (99.34%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -15,17 +15,19 @@ public:
             prev[j] = j;
         }
         for(int i = 1; i <= n1; i++){
-            vector<int> curr(n2 + 1, 0);
-            curr[0] = i; 
+            // vector<int> curr(n2 + 1, 0);
+            int last = prev[0];
+            prev[0] = i; 
             for(int j = 1; j <= n2; j++){
+                int temp = prev[j];
                 if(word1[i - 1] == word2[j - 1]){
-                    curr[j] = prev[j - 1];
+                    prev[j] = last;
                 }
                 else{
-                   curr[j] = 1 + min(curr[j - 1], min(prev[j - 1], prev[j]));
+                   prev[j] = 1 + min(last, min(prev[j - 1], prev[j]));
                 }
+                last = temp;
             }
-            prev = curr;
         }
 
         return prev[n2];

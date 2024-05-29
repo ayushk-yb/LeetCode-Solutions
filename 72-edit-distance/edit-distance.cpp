4ccc9e293bd0ba02313c009aa289b9e4ce7116b7
dotnetbabu <ayush06092002@gmail.com>
2024-05-29 11:10:55 +0530

    Time: 4 ms (82.87%) | Memory: 11.5 MB (79.29%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -6,25 +6,28 @@ public:
         if(n1 == 0 && n2 == 0){
             return 0;
         }
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
-        // return solve(n1 - 1, n2 - 1, word1, word2, dp);
-        for(int i = 0; i <= n1; i++){
-            dp[i][0] = i;
-        }
+        // vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        vector<int> prev(n2 + 1, 0);
+        // for(int i = 0; i <= n1; i++){
+        //     dp[i][0] = i;
+        // }
         for(int j = 0; j <= n2; j++){
-            dp[0][j] = j;
+            prev[j] = j;
         }
         for(int i = 1; i <= n1; i++){
+            vector<int> curr(n2 + 1, 0);
+            curr[0] = i; 
             for(int j = 1; j <= n2; j++){
                 if(word1[i - 1] == word2[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 }
                 else{
-                    dp[i][j] = 1 + min(dp[i][j - 1], min(dp[i - 1][j - 1], dp[i - 1][j]));
+                   curr[j] = 1 + min(curr[j - 1], min(prev[j - 1], prev[j]));
                 }
             }
+            prev = curr;
         }
 
-        return dp[n1][n2];
+        return prev[n2];
     }
 };

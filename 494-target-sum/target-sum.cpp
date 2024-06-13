7c57338fccdc5e12ba112e690a15dca2f3e0d221
dotnetbabu <ayush06092002@gmail.com>
2024-06-13 22:26:36 +0530

    Time: 10 ms (62.36%) | Memory: 24.4 MB (26.05%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
-    int countPartitions(int n, int d, vector<int> &arr) {
-        int totalSum = 0;
-        for(int i = 0; i < n; i++)
-        {
-            totalSum += arr[i];
-        }
-
-        int diff = totalSum - d;
-        if(diff % 2 != 0 || diff < 0)
-            return 0;
-        
-        int k = diff / 2;
-
-        vector<vector<int>> dp(n, vector<int>(k + 1, 0));
-        if(arr[0] == 0)
-            dp[0][0] = 2;
-        else
-            dp[0][0] = 1;
-        
-        if(arr[0] != 0 && arr[0] <= k)
-            dp[0][arr[0]] = 1;
-
-        for(int i = 1; i < n; i++)
-        {
-            for(int j = 0; j <= k; j++)
-            {
-                int not_take = dp[i - 1][j];
-                int take = 0;
-                if(arr[i] <= j)
-                    take = dp[i - 1][j - arr[i]];
-                
-                dp[i][j] = (take + not_take);
+    int solve(vector<int> &nums, int i, int target, int &n, int &sum, vector<vector<int>> &dp){
+        if(i == n){
+            if(target == 0){
+                return 1;
             }
+            return 0;
         }
 
-        return dp[n - 1][k];
+        if(dp[i][target + sum] != -1){
+            return dp[i][target + sum];
+        }
+
+        return dp[i][target + sum] = solve(nums, i + 1, target + nums[i], n, sum, dp) +
+                solve(nums, i + 1, target - nums[i], n, sum, dp);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return countPartitions(nums.size(), target, nums);
+        int n = nums.size();
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+        }
+
+        if(abs(target) > sum){
+            return 0;
+        }
+        vector<vector<int>> dp(n, vector<int>(target + 2 * sum + 1, -1));
+
+        return solve(nums, 0, target, n, sum, dp);
     }
 };

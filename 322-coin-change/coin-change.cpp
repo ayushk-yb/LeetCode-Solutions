77837599e23056c150c3ef4bb3746a0395fcab16
dotnetbabu <ayush06092002@gmail.com>
2024-06-17 19:04:27 +0530

    Time: 39 ms (46.36%) | Memory: 20.1 MB (45.26%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 1e9));
+        // vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 1e9));
         // int res = solve(0, n, coins, amount, dp);
+        vector<int> next(amount + 1, 1e9);
+        vector<int> curr(amount + 1, 1e9);
         for(int j = 0; j <= amount; j++){
             if(j % coins[n - 1] == 0){
-                dp[n - 1][j] = j / coins[n - 1];
+                next[j] = j / coins[n - 1];
             }
         }
         for(int i = n - 2; i >= 0; i--){
             for(int j = 0; j <= amount; j++){
                 int take = 1e9;
                 if(j >= coins[i]){
-                    take = 1 + dp[i][j - coins[i]];
+                    take = 1 + curr[j - coins[i]];
                 }
-                int notTake = dp[i + 1][j];
+                int notTake = next[j];
 
-                dp[i][j] = min(take, notTake);              
+                curr[j] = min(take, notTake);              
             }
+            next = curr;
         }
-        int res = dp[0][amount];
+        int res = next[amount];
         return (res == 1e9) ? -1 : res;
     }
 };

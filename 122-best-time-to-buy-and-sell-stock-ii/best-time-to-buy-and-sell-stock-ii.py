86da01d888a4c91883f6c686fa165c942d2d9887
dotnetbabu <ayush06092002@gmail.com>
2024-10-05 19:26:23 +0530

    Time: 38 ms (63.70%) | Memory: 12.7 MB (59.13%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,21 +1,14 @@
 class Solution(object):
-    def solve(self, i, prices, buy_flag, dp):
-        if i == len(prices):
-            return 0
-        if dp[i][buy_flag] != None:
-            return dp[i][buy_flag]
-        if buy_flag:
-            buy = self.solve(i + 1, prices, 0, dp) - prices[i]
-            not_buy = self.solve(i + 1, prices, 1, dp)
-            dp[i][buy_flag] = max(buy, not_buy)
-            return dp[i][buy_flag]
-        else:
-            sell = self.solve(i + 1, prices, 1, dp) + prices[i]
-            not_sell = self.solve(i + 1, prices, 0, dp)
-            dp[i][buy_flag] = max(sell, not_sell)
-            return dp[i][buy_flag]
-
     def maxProfit(self, prices):
-        dp = [[None] * 2 for _ in range(len(prices))] 
-        return self.solve(0, prices, 1, dp)
+        maxProf = 0
+        buy = float('inf')
+
+        for i in range(len(prices)):
+            if buy < prices[i]:
+                maxProf += prices[i] - buy
+                buy = prices[i]
+            else:
+                buy = prices[i]
+            
+        return maxProf
         

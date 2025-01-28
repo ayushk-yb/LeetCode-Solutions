63cbf87ef4e74f1f6e4c9eaaf3ebe7591eb92e65
dotnetbabu <ayush06092002@gmail.com>
2025-01-28 12:56:27 +0530

    Time: 0 ms (100%), Space: 20.4 MB (18.85%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,23 +1,37 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        count, result = 0, None
-        def solve(node):
-            nonlocal count, result
-            if not node or result != None:
-                return
-            
-            solve(node.left)
-            count += 1
-            if count == k:
-                result = node.val
-            
-            solve(node.right)
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: Optional[TreeNode]
+        :type k: int
+        :rtype: int
+        """
+        curr = root
+        while curr:
+            if not curr.left:
+                k -= 1
+                if k == 0:
+                    return curr.val
+                curr = curr.right
+            else:
+                pre = curr.left
+                while pre.right and pre.right != curr:
+                    pre = pre.right
+                
+                if pre.right == None:
+                    pre.right = curr
+                    curr = curr.left
+                else:
+                    pre.right = None
+                    k -= 1
+                    if k == 0:
+                        return curr.val
+                    
+                    curr = curr.right
         
-        solve(root)
-        return result
+        return -1

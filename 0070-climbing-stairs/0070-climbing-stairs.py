ff98cc9821f01968dbece8b9a534b8610dff3787
dotnetbabu <ayush06092002@gmail.com>
2025-05-29 10:54:21 +0530

    Time: 0 ms (100%), Space: 12.3 MB (80.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -7,20 +7,26 @@ class Solution(object):
         if n == 1 or n == 2:
             return n
 
-        dp = defaultdict(int)
-        dp = {i : -1 for i in range(n + 1)}
+        # dp = defaultdict(int)
+        dp = {i : 1 for i in range(n + 1)}
 
-        def climbWays(numStairs):
-            if numStairs == 1 or numStairs == 2:
-                return numStairs
-            if dp[numStairs] != -1:
-                return dp[numStairs]
+        # def climbWays(numStairs):
+        #     if numStairs == 1 or numStairs == 2:
+        #         return numStairs
+        #     if dp[numStairs] != -1:
+        #         return dp[numStairs]
 
-            oneStep = climbWays(numStairs - 1)
-            twoSteps = climbWays(numStairs - 2)
+        #     oneStep = climbWays(numStairs - 1)
+        #     twoSteps = climbWays(numStairs - 2)
 
-            dp[numStairs] = oneStep + twoSteps
+        #     dp[numStairs] = oneStep + twoSteps
 
-            return dp[numStairs]
+        #     return dp[numStairs]
         
-        return climbWays(n)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
